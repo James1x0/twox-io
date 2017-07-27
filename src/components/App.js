be42e58f9a15eed41c5ebdabaae7cab87438e966
James-Mac <james@aehr.org>
2017-07-27 17:58:32 -0600

    Init

---
+++ src/components/App.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react'
+import Waypoint from 'react-waypoint'
+import classNames from 'classnames'
+import Navigation from './layout/Navigation'
+import Contact from './layout/Contact'
+import Hero from './display/Hero'
+import './App.css'
+
+class App extends Component {
+  constructor () {
+    super(...arguments)
+    this._handleEnter = this._handleEnter.bind(this)
+    this._handleLeave = this._handleLeave.bind(this)
+  }
+
+  componentDidMount () {
+    console.log(`
+        -----------------------------------------------------------------
+        Hello there!
+        Hopefully there's nothing to see here. ¯\\_(ツ)_/¯
+        -----------------------------------------------------------------
+    `)
+  }
+
+  render () {
+    const {
+      animateInIndexHero,
+      animateInStory,
+      animateInContact
+    } = this.state || {}
+
+    return (
+      <div className='application__container'>
+        <Navigation location={this.props.location} />
+
+        <Waypoint
+          onEnter={this._handleEnter('IndexHero')}
+          onLeave={this._handleLeave('IndexHero')}
+        >
+          <div>
+            <Hero
+              className={animateInIndexHero ? 'animate-in' : ''}
+              content='Building & consulting the future of the digital world.'
+            />
+          </div>
+        </Waypoint>
+
+        <section className={
+          classNames('story__developer', {
+            'animate-in': animateInStory
+          })}
+        >
+          <Waypoint
+            onEnter={this._handleEnter('Story')}
+            onLeave={this._handleLeave('Story')}
+          >
+            <div className='story__content'>
+              <h1><span>James Collins</span> | <span>Founder + Developer</span></h1>
+              <p><strong>Meet the developer</strong>. James comes from a broad background of tech and has over <strong>6 years</strong> in professional software development experience. He has built elegant <strong>full-stack</strong> solutions with frameworks like React/Ember and Node+Koa/Express.</p>
+            </div>
+          </Waypoint>
+        </section>
+
+        <Waypoint
+          onEnter={this._handleEnter('Contact')}
+          onLeave={this._handleLeave('Contact')}
+          bottomOffset='200px'
+        >
+          <div>
+            <Contact animateIn={animateInContact} />
+          </div>
+        </Waypoint>
+      </div>
+    )
+  }
+
+  _handleEnter (context) {
+    return () => {
+      this.setState({
+        [`animateIn${context}`]: true
+      })
+    }
+  }
+
+  _handleLeave (context) {
+    return () => {
+      this.setState({
+        [`animateIn${context}`]: false
+      })
+    }
+  }
+}
+
+export default App
---
+++ src/components/layout/Contact.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import classNames from 'classnames'
+import './Contact.css'
+
+const HEADER = 'Get in touch'
+
+class Contact extends Component {
+  render () {
+    const letters = HEADER.split('').map((l, i) => (
+      <i
+        key={i}
+        style={{
+          transition: `${i > 0 ? i / 8 : 0}s opacity ${1 / 8}s ease-in-out`
+        }}
+      >
+        {l}
+      </i>
+    ))
+
+    return (
+      <section className={classNames('contact', {
+        'animate-in': this.props.animateIn
+      })}>
+        <div className='contact__content'>
+          <h1>{letters}</h1>
+          <h2 className='contact__email'><a href='mailto:james@twox.io'>james@twox.io</a></h2>
+          <h2 className='contact__tel'><a href='tel:6055937302'>(605) 593-7302</a></h2>
+        </div>
+      </section>
+    )
+  }
+}
+
+Contact.propTypes = {
+  animateIn: React.PropTypes.bool
+}
+
+export default Contact
